eecfc236b8f890f1936c863bd1d1bc8d6222bea8
Pallab Maji <67859246+codesteller@users.noreply.github.com>
2024-03-13 15:53:50 +0530

    Update noetic_launch.sh

--- noetic_launch.sh
+++ noetic_launch.sh
@@ -4,4 +4,4 @@ xhost +local:docker
 docker run --rm -it -v $(pwd)/aras_sensor_interfaces:/workspace/bike_ws -v /dev/:/dev/ --privileged -e DISPLAY \
 -p 32925:32925 -p 11311:11311 -p 8888:8888 -p 8765:8765 \
 -v /tmp/.X11-unix:/tmp/.X11-unix -w /workspace/bike_ws \
---name ros_noetic codesteller/ros-noetic-base:24.03-arm64 bash
+--name ros_noetic codesteller/ros-noetic-base:24.03-amd64 bash
